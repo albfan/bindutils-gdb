21e489b171754ecd6e7e59d4cdb57a0de4701c1b
albfan <albertofanjul@gmail.com>
2016-10-09 11:10:29 +0200

    Use toolchain to compile

--- test/test_backtrace.sh
+++ test/test_backtrace.sh
@@ -4,10 +4,9 @@ test_description="Testing backtrace output"
 
 . ./sharness/sharness.sh
 
-gcc ../sighandler.c -g -o sighandler -rdynamic
-
 test_expect_success "Compare backtrace" "
-    ./sighandler | sed \"s+$PWD/++g\" | sed '/__libc_start_main/d' > sighandler.output
+    ln -s ../sighandler sighandler
+    ./sighandler | sed \"s+$(dirname \\"$PWD\\")/++g\" | sed '/__libc_start_main/d' > sighandler.output
     diff sighandler.output ../sighandler.result
 "
 
